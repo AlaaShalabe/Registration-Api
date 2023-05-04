482a8f2ae7a15f392496d4dd767b64473868fee4
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-05-04 12:57:51 +0300

    delete logout Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LogoutController;
 use App\Http\Controllers\API\RegistrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('\register', [RegistrationController::class, 'registration']);
 Route::post('\login', [LoginController::class, 'login']);

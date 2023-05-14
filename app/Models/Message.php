3134b6a6a507bc4b251df1a5b002a42a2c73a943
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-05-14 20:44:51 +0300

    make models and migration that need for Chat app

---
+++ app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+    public function chat()
+    {
+        return $this->belongsTo(Chat::class);
+    }
+    public function sender()
+    {
+        return $this->belongsTo(User::class,'sender_id');
+    }
+    public function reciver()
+    {
+       return $this->belongsTo(User::class,'reciver_id');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\LoginController;
-use App\Http\Controllers\API\RegistrationController;
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\RegistrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 

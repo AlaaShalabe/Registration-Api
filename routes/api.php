7aa305cfb87ad22f6e036b11920b1f1c928a25ff
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-05-14 21:11:37 +0300

    webSocket configuration

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegistrationController::class, 'registration']);
 Route::post('/login', [LoginController::class, 'login']);
+
